ccd2a8f2d3726996901fe664b4e33507ea3a5444
seaan <seanwidmier@gmail.com>
2019-10-20 10:28:20 +0100

    add logging

--- mav.py
+++ mav.py
@@ -10,53 +10,58 @@ GPIO_LED = 26
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(GPIO_LED, GPIO.OUT)
 
-# parser = argparse.ArgumentParser()
-# parser.add_argument('--connect')
-# args = parser.parse_args()
+if __name__ == '__main__':
+    port = '/dev/serial0'
 
-port = '/dev/serial0'  # args.connect
+    # Connect to the Vehicle.
+    vehicle = connect(port, wait_ready=True, baud=921600)
 
-# Connect to the Vehicle.
-vehicle = connect(port, wait_ready=True, baud=921600)
+    # print vehicle state
+    print("Get some vehicle attribute values:")
+    print(" GPS: %s" % vehicle.gps_0)
+    print(" Battery: %s" % vehicle.battery)
+    print(" Last Heartbeat: %s" % vehicle.last_heartbeat)
+    print(" Is Armable?: %s" % vehicle.is_armable)
+    print(" System status: %s" % vehicle.system_status.state)
+    print(" Mode: %s" % vehicle.mode.name)    # settable
 
-# print vehicle state
-print("Get some vehicle attribute values:")
-print(" GPS: %s" % vehicle.gps_0)
-print(" Battery: %s" % vehicle.battery)
-print(" Last Heartbeat: %s" % vehicle.last_heartbeat)
-print(" Is Armable?: %s" % vehicle.is_armable)
-print(" System status: %s" % vehicle.system_status.state)
-print(" Mode: %s" % vehicle.mode.name)    # settable
+    try:
+        with open('log.txt', 'a+') as outf:
+            while True:
+                if vehicle.groundspeed < 0.5:
+                    
+                    print("stopped ", vehicle.groundspeed)
 
-while True:
-    if vehicle.groundspeed < 0.5:
-        
-        print("stopped ", vehicle.groundspeed)
+                    total = 0
+                    count = 0
+                    for i in range(0, 10):
+                        dist = distance()
+                        if dist:
+                            total += dist
+                            count += 1
+                        time.sleep(.01)
 
-        total = 0
-        count = 0
-        for i in range(0, 10):
-            dist = distance()
-            if dist:
-                total += dist
-                count += 1
-            #print("Measured Distance = %.1f cm" % dist)
-            time.sleep(.01)
+                    if count > 0:
+                        sample = total/count
+                    
+                        if is_ice(sample):
+                            print("Ice.", sample)
+                            GPIO.output(GPIO_LED, True)
+                            outf.write("Ice found at (" + str(vehicle.location._lat) + ", " + str(vehicle.location ._lon) + ")\n")
+                        else:
+                            print("Not ice.", sample)
+                            GPIO.output(GPIO_LED, False)
+                    else:
+                        print("unable to collect data!")
+                else:
+                    GPIO.output(GPIO_LED, False)
+                    print("Moving", vehicle.groundspeed)
+                time.sleep(.5)
 
-        sample = total/count
-	    
-        if is_ice(sample):
-            print("Ice.", sample)
-            GPIO.output(GPIO_LED, True)
-        else:
-            print("Not ice.", sample)
-            GPIO.output(GPIO_LED, False)
-    else:
-        GPIO.output(GPIO_LED, False)
-        print("Moving", vehicle.groundspeed)
-    time.sleep(.5)
+    except KeyboardInterrupt:
+        # Close vehicle object before exiting script
+        vehicle.close()
 
-# Close vehicle object before exiting script
-vehicle.close()
+        GPIO.cleanup()
 
-print("Completed")
+        print("Completed")
