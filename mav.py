400a85851c9199653471542e0528b05ba11e0e56
seaan <seanwidmier@gmail.com>
2019-10-20 10:59:55 +0100

    add servo control

--- mav.py
+++ mav.py
@@ -6,11 +6,17 @@ from sonic import distance, is_ice
 
 
 GPIO_LED = 26
+GPIO_SRV = 17
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(GPIO_LED, GPIO.OUT)
+
 
 if __name__ == '__main__':
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(GPIO_SRV,GPIO.OUT) 
+    GPIO.setup(GPIO_LED, GPIO.OUT)
+
+    servo = GPIO.PWM(GPIO_SRV, 50)     # Sets up pin 11 as a PWM pin
+    servo.start(0)               # Starts running PWM on the pin and sets it to 0
     port = '/dev/serial0'
 
     # Connect to the Vehicle.
@@ -27,6 +33,7 @@ if __name__ == '__main__':
 
     try:
         with open('log.txt', 'a+') as outf:
+            sample_time = time.time()
             while True:
                 if vehicle.groundspeed < 0.5:
                     
@@ -47,7 +54,15 @@ if __name__ == '__main__':
                         if is_ice(sample):
                             print("Ice.", sample)
                             GPIO.output(GPIO_LED, True)
+                            # log location
                             outf.write("Ice found at (" + str(vehicle.location._lat) + ", " + str(vehicle.location ._lon) + ")\n")
+                            
+                            # move the servo back and forth
+                            if time.time() - sample_time > 5:
+                                servo.ChangeDutyCycle(7)     # move to 90 degrees
+                                time.sleep(2)                # wait 10 secs
+                                servo.ChangeDutyCycle(12)    # move back to zero
+                                sample_time = time.time()
                         else:
                             print("Not ice.", sample)
                             GPIO.output(GPIO_LED, False)
@@ -62,6 +77,8 @@ if __name__ == '__main__':
         # Close vehicle object before exiting script
         vehicle.close()
 
+        # Clean up outputs
+        servo.stop()
         GPIO.cleanup()
 
         print("Completed")
